15a4af596523a7b4ab72866df5145f153df48421
Aslan <hacakirdogan@gmail.com>
2023-09-08 11:47:39 +0300

    filtering added

--- excel-download/webapp/controller/ExcelDownload.controller.js
+++ excel-download/webapp/controller/ExcelDownload.controller.js
@@ -3,11 +3,13 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/ui/export/library",
     "sap/ui/export/Spreadsheet",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
   ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, exportLibrary, Spreadsheet) {
+  function (Controller, exportLibrary, Spreadsheet, Filter, FilterOperator) {
     "use strict";
 
     var EdmType = exportLibrary.EdmType;
@@ -68,6 +70,19 @@ sap.ui.define(
         oSheet.build().finally(function () {
           oSheet.destroy();
         });
+      },
+      onFilterVehicles: function (oEvent) {
+        // build filter array
+        var aFilter = [];
+        var sQuery = oEvent.getParameter("query");
+        if (sQuery) {
+          aFilter.push(new Filter("vehicle", FilterOperator.Contains, sQuery));
+        }
+
+        // filter binding
+        var oList = this.byId("vehicles_tab");
+        var oBinding = oList.getBinding("items");
+        oBinding.filter(aFilter);
       },
     });
   }
